5782b3eb30f88cc12ac9d27803fc93d75562a732
RIcardo Souza <ryszard696@yahoo.com.br>
2022-11-14 17:26:58 -0300

    First

---
+++ Program.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TesteLINQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numeros = new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            #region Método IEnumerable
+
+            ////funcao de filtro que recebe um numero e retorna o que for filtrado
+            //Func<int, bool> funcao = i => i > 2;
+
+            //// resultado com os numeros filtrados pela função.
+            //var resultado = numeros.Where(funcao);
+
+            //foreach (var item in resultado)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            #endregion Método IEnumerable
+
+            #region IQueryable<T>
+
+            //var numeros1 = new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 }.AsQueryable();
+
+            //// função que vai realizar o filtro
+            //Expression<Func<int, bool>> funcao = f => f > 4;
+
+            ////filtro funcao aplicado
+            //var resultado = numeros1.Where(funcao);
+
+            //foreach (var item in resultado)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            #endregion IQueryable<T>
+
+            #region LINQ utilizando estilo de consulta "sintaxe de método" where
+
+            //// filtro que retorna numeros maiores que 5 no ARRAY de numeros.
+            //var numerosFiltrados = numeros.Where(n => n > 5);
+
+            //foreach (var item in numerosFiltrados)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            //// Exemplo para filtrar uma lista de nomes.
+            //var pessoas = new[]
+            //{
+            //    "Carlos Alberto",
+            //    "Maria do Carmo",
+            //    "Maria Antonieta",
+            //    "Hilda Furacão",
+            //    "Bino o Caminhoneiro"
+            //};
+
+            ////filtro
+            //var pessoasFiltradas = pessoas.Where(p => p.Contains("Maria"));
+
+            //foreach (var item in pessoasFiltradas)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            #endregion LINQ utilizando estilo de consulta "sintaxe de método" where
+
+            #region LINQ COUNT
+
+            ////Contagem total
+            //var countTotal = numeros.Count();
+
+            ////Contagem com critérios
+            //var countParcial = numeros.Count(n => n > 5);
+
+            //Console.WriteLine(countTotal);
+            //Console.WriteLine(countParcial);
+
+            #endregion LINQ COUNT
+
+            #region LINQ SUM
+
+            //var somaTotal = numeros.Sum();
+            //var somaComMultiplicacao = numeros.Sum(n => n * 2);
+
+            //Console.WriteLine(somaTotal);
+            //Console.WriteLine(somaComMultiplicacao);
+
+            #endregion LINQ SUM
+
+            #region LINQ – OrderBy and OrderByDescending
+
+            ////ordem crescente
+            //var numerosCrescente = numeros.OrderBy(n => n);
+
+            //foreach (var item in numerosCrescente)
+            //{
+            //    Console.Write(item + ", ");
+            //}
+            //Console.WriteLine();
+            //Console.WriteLine();
+            ////ordem decrescente
+            //var numerosOrdenados = numeros.OrderByDescending(n => n);
+
+            //foreach (var item in numerosOrdenados)
+            //{
+            //    Console.Write(item + ", ");
+            //}
+
+            #endregion LINQ – OrderBy and OrderByDescending
+
+            #region LINQ - Take e TakeWhile
+
+            //// vai buscar até o numero 3
+            //var items = numeros.Take(3);
+
+            //foreach (var num in items)
+            //{
+            //    Console.WriteLine(num);
+            //}
+
+            ////TAKEWHILE busca até encontrar o critério
+            //var itens = numeros.TakeWhile(n => n < 6);
+            //foreach (var item in itens)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            #endregion LINQ - Take e TakeWhile
+
+            #region FIRST e FirstOrDefault
+
+            //var num = numeros.First();
+            //Console.WriteLine(num);
+
+            ////com filtro
+            //var num2 = numeros.First(p => p > 6 && p < 9);
+            //Console.WriteLine(num2);
+
+            #endregion FIRST e FirstOrDefault
+
+            #region Skip
+
+            //var numeros2 = new[] { 1, 2, 3, 3, 3, 4, 5 };
+            //var numerosPaginados = numeros2.Skip(3);
+
+            //foreach (var item in numerosPaginados)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            #endregion Skip
+
+            #region Concat
+
+            //var colecao1 = new[] { 1, 2, 3 };
+            //var colecao2 = new[] { 6, 7, 8 };
+
+            //var numConcat = colecao1.Concat(colecao2);
+
+            //foreach (var item in numConcat)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            #endregion Concat
+
+            #region ElementAt
+
+            //// busca o numero no index 4 = 5
+            //var numero = numeros.ElementAt(4);
+            //Console.WriteLine(numero);
+
+            #endregion ElementAt
+
+            #region Single e SingleOrDefault
+
+            //var numeros2 = new[] { 1, 2, 3, 3, 4, 5, 5, 6 };
+
+            //try
+            //{   //filtro que vai gerar uma exceção pois tem dois 3
+            //    var numero = numeros2.Single(p => p == 3);
+            //    Console.WriteLine(numero);
+            //}
+            //catch (Exception ex)
+            //{
+            //    Console.WriteLine(ex.Message + " existem dois numeros 3 na lib");
+            //}
+
+            //// filtro que não gera exceção pois só tem um numero 6 na biblioteca
+            //var numero1 = numeros2.Single(p => p == 6);
+            //Console.WriteLine("somente um numero " + numero1 + " na lib");
+
+            #endregion Single e SingleOrDefault
+
+            #region Select
+
+            //// p vai receber os numeros e ele será uma nova coleção, com descrição e ID herdada de numeros.
+            //var categorias = numeros.Select(p => new
+            //{
+            //    Id = p,
+            //    Descricao = $"Descricao {p}"
+            //});
+
+            //foreach (var item in categorias)
+            //{
+            //    var descricao = $"Categoria: {item.Id} - {item.Descricao}";
+            //    Console.WriteLine(descricao);
+            //}
+
+            #endregion Select
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,109 @@
+# <center>**ESTUDO DE MÉTODOS LINQ C#**</center>
+
+## <center><mark>OPERADORES SUPORTADOS</mark></center>
+
+ ### Métodos de Projeção e restrição
+ - ##### Select, SelectMany, Where
+<br>
+
+### Métodos  de ordenação
+- ##### OderBy, OrderByDescending, ThenByDescending, ThenBy, Reverse
+<br>
+
+### Métodos de Agregação
+- ##### Average, Count, LongCount, Max, Min, Sum
+<br>
+
+### Métodos de Paginação
+ - ##### First, FirstOrDefault, Last, LastOrDefault, Single,
+ - ##### SingleOrDefault, Skip, SkipWhile, Take, TakeWhile, ElmentAt
+<br>
+
+### Métodos de Junção
+- ##### Join, GroupJoin
+<br>
+
+---
+
+> **Where** - Recebe os critérios que devem ser aplicados em sua consulta, 
+interpreta e filtra os elementos com base nos critérios informados 
+pelo desenvolvedor.
+
+---
+
+> **Count** - Capacidade de efetuar uma contagem de elementos 
+em uma coleção, podemos contar todos elementos ou informar um 
+critério que desejamos aplicar para o LINQ filtrar uma determinada 
+região dos dados em sua coleção.
+
+---
+
+> **Sum** - Fornece a capacidade de efetuar cálculo de soma sobre os elementos da coleção, é limitado a tipos int, double, decimal, float e 
+long
+
+---
+
+> **OrderBy** - Faz a ordenação dos elementos em ordem ascendente (do menor para o maior) respeitando o tipo de objeto da coleção.
+
+---
+
+> **OrderByDescending** - Este operador fornece a capacidade de devolver uma coleção 
+em uma ordem inversa.
+
+---
+
+> **Take** - Devolve uma nova coleção de objetos com a quantidade de elementos informados, respeitando todos critérios 
+de filtros aplicados a sua consulta.
+
+---
+
+> **TakeWhile** - Funciona como um filtro, ele faz iteração na coleção enquanto atender os critérios fornecidos, ou seja, quando a condição for falsa ele devolve os elementos 
+da coleção que atenderam os critérios. Diferente do **Where** que percorre todos elementos até o fim.
+
+---
+
+> **First** - O método de First() retorna o primeiro elemento da coleção, aceita filros e sobrecarga. 
+Quando a biblioteca for nula ele retorna uma exceção do tipo InvalidOperationException. Bugando o programa.
+
+---
+
+> **FirstOrDefault** - Basicamente tem o mesmo comportamento do método First, a única diferença é que não será gerada 
+uma exceção em caso da coleção estar vazia ou não ser encontrado o elemento que 
+atenda ao critério passado na sobrecarga do método.
+
+---
+
+> **Last e LastOrDefault** - O comportamento é o mesmo do First e FirstOrDefault, o que muda 
+é apenas o retorno do elemento, retornando o último elemento da coleção.
+
+---
+
+> **Skip** - O Skip é um recurso muito importante no LINQ fornecendo a 
+capacidade de ler elementos a partir de uma determinada posição dentro da coleção.
+
+---
+
+> **Concat** - Durante o processo de desenvolvimento de software, às vezes 
+nos deparamos com um cenário onde é necessário concatenar 2 coleções.
+
+---
+
+> **ElementAt** - Para cenários onde precisamos recuperar um elemento pelo de 
+índice, o LINQ fornece o método ElementAt.
+
+---
+
+> **Single e SingleOrDefault** - Single e SingleOrDefault são recursos de paginação extremamente importantes, sendo úteis em alguns cenários. Os comportamentos são parecidos com o First e FirstOrDefault, que tem como objetivo retornar um único elemento de uma coleção, mas o Single e 
+SingleOrDefault é mais robusto, ele consegue validar se existe na 
+coleção mais de 1 (um) item que atende os mesmos critérios do filtro, se existir ele vai lançar uma exceção, informando que a coleção 
+contém mais de um elemento.
+
+---
+
+> **Select** - Tem a capacidade de transformar os objetos de sua coleção, em 
+uma nova coleção de objetos formatados, enriquecidos ou mais 
+filtrados, ou seja, você pode criar novos tipos de objetos, fornecendo infinitas possibilidades de 
+manipulação de seus dados.
+
+---
+
